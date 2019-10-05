fe236910cb3483cbdd87a2952d8369d7a5c3e9f8
Ming Fang <mingfang@mac.com>
2019-10-04 20:00:15 -0400

    delete

--- modules/alluxio/saved/csi-attacher-service_account.tf
+++
@@ -1,6 +0,0 @@
-resource "k8s_core_v1_service_account" "csi-attacher" {
-  metadata {
-    name      = "csi-attacher"
-    namespace = var.namespace
-  }
-}
--- modules/alluxio/saved/csi-nodeplugin-service_account.tf
+++
@@ -1,6 +0,0 @@
-resource "k8s_core_v1_service_account" "csi-nodeplugin" {
-  metadata {
-    name      = "csi-nodeplugin"
-    namespace = var.namespace
-  }
-}

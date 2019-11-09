ebaae44e104490b24ae5d2548753fc666a436426
Ming Fang <mingfang@mac.com>
2019-11-09 18:01:22 -0500

    storage-nfs handle empty output

--- modules/kubernetes/storage-nfs/outputs.tf
+++ modules/kubernetes/storage-nfs/outputs.tf
@@ -1,9 +1,9 @@
 output storage_class_name {
-  value = element(k8s_core_v1_persistent_volume_claim.this.*.spec.0.storage_class_name, 0)
+  value = length(k8s_core_v1_persistent_volume_claim.this.*) > 0 ? element(k8s_core_v1_persistent_volume_claim.this.*.spec.0.storage_class_name, 0) : ""
 }
 
 output storage {
-  value = element(k8s_core_v1_persistent_volume_claim.this.*.spec.0.resources.0.requests.storage, 0)
+  value = length(k8s_core_v1_persistent_volume_claim.this.*) > 0 ? element(k8s_core_v1_persistent_volume_claim.this.*.spec.0.resources.0.requests.storage, 0) : ""
 }
 
 output replicas {

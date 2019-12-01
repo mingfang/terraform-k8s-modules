98cf6e35286b0eb47a1bcf357bba77aaad47d286
Ming Fang <mingfang@mac.com>
2019-12-01 07:49:01 -0500

    rewrite postgres module

--- modules/postgres/main.tf
+++ modules/postgres/main.tf
@@ -1,284 +1,87 @@
-/**
- * Module usage:
- *
- *     module "postgres" {
- *       source             = "git::https://github.com/mingfang/terraform-k8s-modules.git//modules/postgres"
- *       name               = "test-postgres"
- *       postgres_user      = "postgres"
- *       postgres_password  = "postgres"
- *       postgres_db        = "postgres"
-
- *       storage_class_name = "test-postgres"
- *       storage            = "100Gi"
- *       replicas           = "${k8s_core_v1_persistent_volume.test-postgres.count}"
- *     }
- *
- *     resource "k8s_core_v1_persistent_volume" "test-postgres" {
- *       count = 1
- *
- *       metadata {
- *         name = "pvc-test-postgres-${count.index}"
- *       }
- *
- *       spec {
- *         storage_class_name               = "test-postgres"
- *         persistent_volume_reclaim_policy = "Retain"
- *
- *         access_modes = [
- *           "ReadWriteMany",
- *         ]
- *
- *         capacity {
- *           storage = "100Gi"
- *         }
- *
- *         cephfs {
- *           user = "admin"
- *
- *           monitors = [
- *             "192.168.2.89",
- *             "192.168.2.39",
- *           ]
- *
- *           secret_ref {
- *             name = "ceph-secret"
- *             namespace = "default"
- *           }
- *         }
- *       }
- *     }
- */
-
-/*
-common variables
-*/
-
-variable "name" {}
-
-variable "namespace" {
-  default = "default"
-}
-
-variable "replicas" {
-  default = 1
-}
-
-variable image {
-  default = "postgres"
-}
-
-variable "node_selector" {
-  type    = "map"
-  default = {}
-}
-
-/*
-statefulset specific
-*/
-
-variable storage_class_name {}
-
-variable storage {}
-
-variable volume_claim_template_name {
-  default = "pvc"
-}
-
-/*
-service specific variables
-*/
-
-variable postgres_password {}
-
-variable postgres_user {}
-
-variable postgres_db {}
-
-variable port {
-  default = 5432
-}
 
 locals {
-  labels = {
-    app     = "${var.name}"
-    name    = "${var.name}"
-    service = "${var.name}"
-  }
-}
+  parameters = {
+    name                 = var.name
+    namespace            = var.namespace
+    annotations          = var.annotations
+    replicas             = var.replicas
+    ports                = var.ports
+    enable_service_links = false
 
-/*
-service
-*/
-
-resource "k8s_core_v1_service" "this" {
-  metadata {
-    name      = "${var.name}"
-    namespace = "${var.namespace}"
-    labels    = "${local.labels}"
-  }
-
-  spec {
-    selector = "${local.labels}"
-
-    ports = [
+    containers = [
       {
-        name = "tcp"
-        port = "${var.port}"
+        name  = "postgres"
+        image = var.image
+
+        env = concat([
+          {
+            name = "POD_NAME"
+
+            value_from = {
+              field_ref = {
+                field_path = "metadata.name"
+              }
+            }
+          },
+          {
+            name  = "POSTGRES_USER"
+            value = var.POSTGRES_USER
+          },
+          {
+            name  = "POSTGRES_PASSWORD"
+            value = var.POSTGRES_PASSWORD
+          },
+          {
+            name  = "POSTGRES_DB"
+            value = var.POSTGRES_DB
+          },
+          {
+            name  = "PGDATA"
+            value = "/data/$(POD_NAME)"
+          },
+        ], var.env)
+
+        volume_mounts = [
+          {
+            name       = var.volume_claim_template_name
+            mount_path = "/data"
+            sub_path   = var.name
+          },
+          {
+            name       = "shm"
+            mount_path = "/dev/shm"
+          },
+        ]
       },
+    ]
+
+    volumes = [
+      {
+        name = "shm"
+
+        empty_dir = {
+          "medium" = "Memory"
+        }
+      },
+    ]
+
+    volume_claim_templates = [
+      {
+        name               = var.volume_claim_template_name
+        storage_class_name = var.storage_class
+        access_modes       = ["ReadWriteOnce"]
+
+        resources = {
+          requests = {
+            storage = var.storage
+          }
+        }
+      }
     ]
   }
 }
 
-/*
-statefulset
-*/
-
-resource "k8s_apps_v1_stateful_set" "this" {
-  metadata {
-    name      = "${var.name}"
-    namespace = "${var.namespace}"
-    labels    = "${local.labels}"
-  }
-
-  spec {
-    replicas              = "${var.replicas}"
-    service_name          = "${k8s_core_v1_service.this.metadata.0.name}"
-    pod_management_policy = "OrderedReady"
-
-    selector {
-      match_labels = "${local.labels}"
-    }
-
-    update_strategy {
-      type = "RollingUpdate"
-
-      rolling_update {
-        partition = 0
-      }
-    }
-
-    template {
-      metadata {
-        labels = "${local.labels}"
-      }
-
-      spec {
-        node_selector = "${var.node_selector}"
-
-        affinity {
-          pod_anti_affinity {
-            required_during_scheduling_ignored_during_execution {
-              label_selector {
-                match_expressions {
-                  key      = "app"
-                  operator = "In"
-                  values   = ["${var.name}"]
-                }
-              }
-
-              topology_key = "kubernetes.io/hostname"
-            }
-          }
-        }
-
-        containers = [
-          {
-            name  = "postgres"
-            image = "${var.image}"
-
-            env = [
-              {
-                name  = "POSTGRES_USER"
-                value = "${var.postgres_user}"
-              },
-              {
-                name  = "POSTGRES_PASSWORD"
-                value = "${var.postgres_password}"
-              },
-              {
-                name  = "POSTGRES_DB"
-                value = "${var.postgres_db}"
-              },
-              {
-                name = "POD_NAME"
-
-                value_from = {
-                  field_ref = {
-                    field_path = "metadata.name"
-                  }
-                }
-              },
-              {
-                name  = "PGDATA"
-                value = "/data/$(POD_NAME)"
-              },
-            ]
-
-            volume_mounts = [
-              {
-                name       = "${var.volume_claim_template_name}"
-                mount_path = "/data"
-                sub_path   = "${var.name}"
-              },
-              {
-                name       = "shm"
-                mount_path = "/dev/shm"
-              },
-            ]
-          },
-        ]
-
-        security_context {}
-
-        volumes = [
-          {
-            name = "shm"
-
-            empty_dir = {
-              "medium" = "Memory"
-            }
-          },
-        ]
-      }
-    }
-
-    volume_claim_templates {
-      metadata {
-        name = "${var.volume_claim_template_name}"
-      }
-
-      spec {
-        storage_class_name = "${var.storage_class_name}"
-        access_modes       = ["ReadWriteOnce"]
-
-        resources {
-          requests {
-            storage = "${var.storage}"
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "k8s_policy_v1beta1_pod_disruption_budget" "this" {
-  metadata {
-    name = "${var.name}"
-  }
-
-  spec {
-    max_unavailable = 1
-
-    selector {
-      match_labels = "${local.labels}"
-    }
-  }
-}
-
-output "name" {
-  value = "${k8s_core_v1_service.this.metadata.0.name}"
-}
-
-output "port" {
-  value = "${k8s_core_v1_service.this.spec.0.ports.0.port}"
+module "statefulset-service" {
+  source     = "../../archetypes/statefulset-service"
+  parameters = merge(local.parameters, var.overrides)
 }
---
+++ modules/postgres/variables.tf
@@ -0,0 +1,48 @@
+variable "name" {}
+
+variable "namespace" {
+  default = null
+}
+
+variable "replicas" {
+  default = 1
+}
+
+variable "ports" {
+  default = [
+    {
+      name = "tcp"
+      port = 5432
+    },
+  ]
+}
+
+variable "image" {
+  default = "postgres:12.1"
+}
+
+variable "env" {
+  default = []
+}
+
+variable "annotations" {
+  default = {}
+}
+
+variable "overrides" {
+  default = {}
+}
+
+variable "storage" {}
+
+variable "storage_class" {}
+
+variable "volume_claim_template_name" {
+  default = "pvc"
+}
+
+variable POSTGRES_USER {}
+
+variable POSTGRES_PASSWORD {}
+
+variable POSTGRES_DB {}

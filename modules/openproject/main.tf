6a06930d055694c7e3986be22ecff2f8a56c92df
Ming Fang <mingfang@mac.com>
2020-04-25 11:43:36 -0400

    add openproject

---
+++ modules/openproject/main.tf
@@ -0,0 +1,99 @@
+
+
+locals {
+  env = concat([
+    {
+      name = "POD_NAME"
+
+      value_from = {
+        field_ref = {
+          field_path = "metadata.name"
+        }
+      }
+    },
+    {
+      name  = "DATABASE_URL"
+      value = var.DATABASE_URL
+    },
+    {
+      name  = "USE_PUMA"
+      value = var.USE_PUMA
+    },
+    {
+      name  = "IMAP_ENABLED"
+      value = var.IMAP_ENABLED
+    },
+    {
+      name  = "RAILS_CACHE_STORE"
+      value = var.RAILS_CACHE_STORE
+    },
+    {
+      name  = "OPENPROJECT_CACHE__MEMCACHE__SERVER"
+      value = var.OPENPROJECT_CACHE__MEMCACHE__SERVER
+    },
+    {
+      name  = "OPENPROJECT_RAILS__RELATIVE__URL__ROOT"
+      value = var.OPENPROJECT_RAILS__RELATIVE__URL__ROOT
+    },
+  ], var.env)
+
+  parameters = {
+    name                 = var.name
+    namespace            = var.namespace
+    annotations          = var.annotations
+    replicas             = var.replicas
+    ports                = var.ports
+    enable_service_links = false
+
+    containers = [
+      {
+        name    = "openproject"
+        image   = var.image
+        command = ["/app/docker/web"]
+        env     = local.env
+
+        volume_mounts = [
+          {
+            name       = "data"
+            mount_path = "/var/openproject/assets"
+          },
+        ]
+      },
+      {
+        name    = "worker"
+        image   = var.image
+        command = ["/app/docker/worker"]
+        env     = local.env
+      },
+      {
+        name    = "cron"
+        image   = var.image
+        command = ["/app/docker/cron"]
+        env     = local.env
+      },
+    ]
+
+    init_containers = [
+      {
+        name    = "seeder"
+        image   = var.image
+        command = ["/app/docker/seeder"]
+        env     = local.env
+      },
+    ]
+
+    volumes = [
+      {
+        name = "data"
+        persistent_volume_claim = {
+          claim_name = var.pvc_name
+        },
+      }
+    ]
+  }
+}
+
+module "deployment-service" {
+  source     = "../../archetypes/deployment-service"
+  parameters = merge(local.parameters, var.overrides)
+}
---
+++ modules/openproject/variables.tf
@@ -0,0 +1,60 @@
+variable "name" {}
+
+variable "namespace" {}
+
+variable "replicas" {
+  default = 1
+}
+
+variable "ports" {
+  default = [
+    {
+      name = "http"
+      port = 8080
+    },
+  ]
+}
+
+variable "image" {
+  default = "openproject/community:10.5.0"
+}
+
+variable "env" {
+  default = []
+}
+
+variable "annotations" {
+  default = {}
+}
+
+variable "overrides" {
+  default = {}
+}
+
+variable "pvc_name" {
+  description = "Volume for /var/openproject/assets"
+}
+
+variable "DATABASE_URL" {
+  description = "postgres://postgres:p4ssw0rd@db/openproject"
+}
+
+variable "USE_PUMA" {
+  default = "true"
+}
+
+variable "IMAP_ENABLED" {
+  default = "false"
+}
+
+variable "RAILS_CACHE_STORE" {
+  default     = null
+  description = "memcache"
+}
+variable "OPENPROJECT_CACHE__MEMCACHE__SERVER" {
+  default     = null
+  description = "cache:11211"
+}
+variable "OPENPROJECT_RAILS__RELATIVE__URL__ROOT" {
+  default = null
+}

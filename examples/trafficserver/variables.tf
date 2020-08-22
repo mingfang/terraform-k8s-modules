c50cc5ce98a82126725ef39b61e5c5b11e9d1774
Ming Fang <mingfang@mac.com>
2020-08-22 10:23:45 -0400

    add trafficserver module

---
+++ examples/trafficserver/variables.tf
@@ -0,0 +1,7 @@
+variable "name" {
+  default = "trafficserver"
+}
+
+variable "namespace" {
+  default = "trafficserver-example"
+}
---
+++ modules/trafficserver/variables.tf
@@ -0,0 +1,40 @@
+variable "name" {}
+
+variable "namespace" {}
+
+variable "image" {
+  default = "registry.rebelsoft.com/trafficserver"
+}
+
+variable "replicas" {
+  default = 1
+}
+
+variable ports {
+  default = [
+    {
+      name = "http"
+      port = 8080
+    },
+  ]
+}
+
+variable "host_port" {
+  default = null
+}
+
+variable "env" {
+  default = []
+}
+
+variable "annotations" {
+  default = {}
+}
+
+variable "resources" {
+  default = {}
+}
+
+variable "overrides" {
+  default = {}
+}

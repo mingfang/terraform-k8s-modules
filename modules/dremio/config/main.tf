d197f014c2e8a1b4cd2dc5080676d43197ad8d53
Ming Fang <mingfang@mac.com>
2020-10-23 00:19:57 -0400

    dremio - upgrade to v4.9.0

--- modules/dremio/config/main.tf
+++ modules/dremio/config/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    k8s = {
-      source  = "mingfang/k8s"
-    }
-  }
-}
-
 resource "k8s_core_v1_config_map" "this" {
   data = {
     "core-site.xml"      = <<-EOF
---
+++ modules/dremio/config/versions.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_providers {
+    k8s = {
+      source  = "mingfang/k8s"
+    }
+  }
+}

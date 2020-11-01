e2447f6dcd0a298476371bc8bf293efff2f90390
Ming Fang <mingfang@mac.com>
2020-10-31 22:30:22 -0400

    add druid

---
+++ examples/druid/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  default = "druid"
+}
+
+variable "namespace" {
+  default = "druid-example"
+}
+
+variable "minio_access_key" {
+  default = "IUWU60H2527LP7DOYJVP"
+}
+
+variable "minio_secret_key" {
+  default = "bbdGponYV5p9P99EsasLSu4K3SjYBEcBLtyz7wbm"
+}

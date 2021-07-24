0d63f1f58e4a7666cdd218c2dac2dc83b9529bdb
Ming Fang <mingfang@mac.com>
2021-07-24 15:26:30 +0000

    add httpbin example

---
+++ examples/httpbin/main.tf
@@ -0,0 +1,42 @@
+resource "k8s_core_v1_namespace" "this" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+module "httpbin" {
+  source    = "../../modules/httpbin"
+  name      = var.name
+  namespace = k8s_core_v1_namespace.this.metadata[0].name
+  replicas  = var.replicas
+}
+
+resource "k8s_networking_k8s_io_v1beta1_ingress" "this" {
+  metadata {
+    annotations = {
+      "kubernetes.io/ingress.class"              = "nginx"
+      "nginx.ingress.kubernetes.io/server-alias" = "${var.namespace}.*"
+      "nginx.ingress.kubernetes.io/ssl-redirect" = "true"
+
+      "nginx.ingress.kubernetes.io/auth-url"              = "https://oauth.rebelsoft.com/oauth2/auth"
+      "nginx.ingress.kubernetes.io/auth-signin"           = "https://oauth.rebelsoft.com/oauth2/start?rd=https://$host$escaped_request_uri"
+      "nginx.ingress.kubernetes.io/auth-response-headers" = "X-Auth-Request-User, X-Auth-Request-Email"
+    }
+    name      = var.name
+    namespace = k8s_core_v1_namespace.this.metadata[0].name
+  }
+  spec {
+    rules {
+      host = "${var.name}-${var.namespace}"
+      http {
+        paths {
+          backend {
+            service_name = module.httpbin.service.metadata[0].name
+            service_port = module.httpbin.service.spec[0].ports[0].port
+          }
+          path = "/"
+        }
+      }
+    }
+  }
+}

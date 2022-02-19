98967b4e80fa37372baa499a6cb8008e66073b1d
Ming Fang <mingfang@mac.com>
2022-02-19 01:09:52 +0000

    add mongo-express

---
+++ modules/mongo-express/main.tf
@@ -0,0 +1,48 @@
+locals {
+  parameters = {
+    name                 = var.name
+    namespace            = var.namespace
+    annotations          = var.annotations
+    replicas             = var.replicas
+    ports                = var.ports
+    enable_service_links = false
+
+    containers = [
+      {
+        name  = "mongo-express"
+        image = var.image
+
+        env = concat([
+          {
+            name  = "ME_CONFIG_MONGODB_ADMINUSERNAME"
+            value = var.ME_CONFIG_MONGODB_ADMINUSERNAME
+          },
+          {
+            name  = "ME_CONFIG_MONGODB_ADMINPASSWORD"
+            value = var.ME_CONFIG_MONGODB_ADMINPASSWORD
+          },
+          {
+            name  = "ME_CONFIG_MONGODB_URL"
+            value = var.ME_CONFIG_MONGODB_URL
+          },
+        ], var.env)
+
+        resources = var.resources
+
+        liveness_probe = {
+          http_get = {
+            path = "/"
+            port = var.ports[0].port
+          }
+          initial_delay_seconds = 30
+          period_seconds        = 5
+        }
+      },
+    ]
+  }
+}
+
+module "deployment-service" {
+  source     = "../../archetypes/deployment-service"
+  parameters = merge(local.parameters, var.overrides)
+}
---
+++ modules/mongo-express/variables.tf
@@ -0,0 +1,46 @@
+variable "name" {}
+
+variable "namespace" {}
+
+variable "image" {
+  default = "mongo-express"
+}
+
+variable "replicas" {
+  default = 1
+}
+
+variable ports {
+  default = [
+    {
+      name = "http"
+      port = 8081
+    },
+  ]
+}
+
+variable "env" {
+  default = []
+}
+
+variable "annotations" {
+  default = {}
+}
+
+variable "resources" {
+  default = {}
+}
+
+variable "overrides" {
+  default = {}
+}
+
+variable "ME_CONFIG_MONGODB_ADMINUSERNAME" {
+  default = ""
+}
+variable "ME_CONFIG_MONGODB_ADMINPASSWORD" {
+  default = ""
+}
+variable "ME_CONFIG_MONGODB_URL" {
+  description = "mongodb://root:example@mongo:27017/"
+}

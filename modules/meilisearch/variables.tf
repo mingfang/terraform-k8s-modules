f39e52dd36c3d19ac816f215871192fb663b161a
Ming Fang <mingfang@mac.com>
2022-03-10 13:39:13 +0000

    add meilisearch

---
+++ modules/meilisearch/variables.tf
@@ -0,0 +1,51 @@
+variable "name" {
+  default = "meilisearch"
+}
+
+variable "namespace" {}
+
+variable "ports" {
+  default = [
+    {
+      name = "tcp"
+      port = 7700
+    },
+  ]
+}
+
+variable "replicas" {
+  default = 1
+}
+
+variable "image" {
+  default = "getmeili/meilisearch:v0.25.2"
+}
+
+variable "env" {
+  default = []
+}
+
+variable "annotations" {
+  default = {}
+}
+
+variable "resources" {
+  default = {
+    requests = {
+      cpu    = "100m"
+      memory = "128Mi"
+    }
+  }
+}
+
+variable "overrides" {
+  default = {}
+}
+
+variable "storage" {}
+
+variable "storage_class" {}
+
+variable "volume_claim_template_name" {
+  default = "pvc"
+}

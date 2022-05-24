2adf39b19ce8092d06bb0d7500ade6eaeef27333
Ming Fang <mingfang@mac.com>
2022-05-24 03:41:30 +0000

    kubernetes-dashboard - upgrade v2.5.0

--- modules/kubernetes/dashboard/variables.tf
+++ modules/kubernetes/dashboard/variables.tf
@@ -3,7 +3,7 @@ variable "name" {}
 variable "namespace" {}
 
 variable "image" {
-  default = "kubernetesui/dashboard:v2.0.4"
+  default = "kubernetesui/dashboard:v2.5.0"
 }
 
 variable "replicas" {

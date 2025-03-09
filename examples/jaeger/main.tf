5f21b7adb0091661ee5abde869eeeef434d74405
Ming Fang <mingfang@mac.com>
2025-03-09 06:29:10 +0000

    update jaeger

--- examples/jaeger/main.tf
+++ examples/jaeger/main.tf
@@ -1,84 +1,49 @@
-resource "k8s_core_v1_namespace" "this" {
-  metadata {
-    name = var.namespace
+module "namespace" {
+  source    = "../namespace"
+  name      = var.namespace
+  is_create = var.is_create_namespace
+}
+
+module "jaeger" {
+  source    = "../../modules/generic-deployment-service"
+  name      = var.name
+  namespace = module.namespace.name
+  image     = "jaegertracing/jaeger:2.3.0"
+  ports = [
+    { name = "tcp1", port = 16686 },
+    { name = "tcp2", port = 4317 },
+    { name = "tcp3", port = 4318 },
+  ]
+
+  env_map = {
+    COLLECTOR_OTLP_ENABLED = "true"
   }
 }
 
-module "nfs-server" {
-  source    = "../../modules/nfs-server-empty-dir"
-  name      = "nfs-server"
-  namespace = k8s_core_v1_namespace.this.metadata[0].name
-}
-
-module "cassandra-storage" {
-  source        = "../../modules/kubernetes/storage-nfs"
-  name          = "cassandra"
-  namespace     = k8s_core_v1_namespace.this.metadata[0].name
-  replicas      = 3
-  mount_options = module.nfs-server.mount_options
-  nfs_server    = module.nfs-server.service.spec[0].cluster_ip
-  storage       = "1Gi"
-
-  annotations = {
-    "nfs-server-uid" = module.nfs-server.deployment.metadata[0].uid
-  }
-}
-
-module "cassandra" {
-  source    = "../../modules/cassandra"
-  name      = "cassandra"
-  namespace = k8s_core_v1_namespace.this.metadata[0].name
-
-  replicas      = module.cassandra-storage.replicas
-  storage       = module.cassandra-storage.storage
-  storage_class = module.cassandra-storage.storage_class_name
-
-  CASSANDRA_CLUSTER_NAME = "jaeger"
-}
-
-module "job" {
-  source    = "../../modules/jaeger/cassandra-schema-job"
-  namespace = k8s_core_v1_namespace.this.metadata[0].name
-}
-
-module "config" {
-  source    = "../../modules/jaeger/cassandra_config"
-  namespace = k8s_core_v1_namespace.this.metadata[0].name
-}
-
-module "collector" {
-  source          = "../../modules/jaeger/collector"
-  name            = "jaeger-collector"
-  namespace       = k8s_core_v1_namespace.this.metadata[0].name
-  config_map_name = module.config.name
-}
-
-module "query" {
-  source          = "../../modules/jaeger/query"
-  name            = "jaeger-query"
-  namespace       = k8s_core_v1_namespace.this.metadata[0].name
-  config_map_name = module.config.name
-}
-
-resource "k8s_networking_k8s_io_v1beta1_ingress" "jaeger" {
+resource "k8s_networking_k8s_io_v1_ingress" "jaeger" {
   metadata {
     annotations = {
       "kubernetes.io/ingress.class"              = "nginx"
-      "nginx.ingress.kubernetes.io/server-alias" = "jaeger.${var.namespace}.*"
+      "nginx.ingress.kubernetes.io/server-alias" = "${var.namespace}.*"
     }
-    name      = "jaeger.${var.namespace}"
-    namespace = k8s_core_v1_namespace.this.metadata[0].name
+    name      = var.namespace
+    namespace = module.namespace.name
   }
   spec {
     rules {
-      host = "jaeger.${var.namespace}"
+      host = var.namespace
       http {
         paths {
           backend {
-            service_name = module.query.name
-            service_port = module.query.service.spec[0].ports[0].port
+            service {
+              name = module.jaeger.name
+              port {
+                number = module.jaeger.ports[0].port
+              }
+            }
           }
-          path = "/"
+          path      = "/"
+          path_type = "ImplementationSpecific"
         }
       }
     }
--- examples/jaeger/variables.tf
+++ examples/jaeger/variables.tf
@@ -5,3 +5,7 @@ variable "name" {
 variable "namespace" {
   default = "jaeger-example"
 }
+
+variable "is_create_namespace" {
+  default = true
+}

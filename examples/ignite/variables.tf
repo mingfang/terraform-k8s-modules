70db0a26d9defa88dc8420ccde6ff7c1cef26e86
Ming Fang <mingfang@mac.com>
2025-04-06 01:41:47 +0000

    add ignite

---
+++ examples/ignite/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  default = "ignite"
+}
+
+variable "namespace" {
+  default = "ignite-example"
+}
+
+variable "is_create_namespace" {
+  default = true
+}
+
+variable "replicas" {
+  default = 3
+}

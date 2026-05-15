571690db4bb4040f77ed11e8b29aa693256b7705
Ming Fang <mingfang@mac.com>
2026-05-15 13:34:42 +0000

    delete

--- modules/cloudnative-pg/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  default     = "cloudnative-pg"
-  description = "Release name for the CloudNativePG operator. Used for label identification."
-}
-
-variable "operator_version" {
-  default     = "1.29.0"
-  description = "CloudNativePG operator version to install. See https://github.com/cloudnative-pg/cloudnative-pg/releases"
-}

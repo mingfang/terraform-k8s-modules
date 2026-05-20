d7b4dbe96fd49a0737cbf15998a9529e78e27c6e
Ming Fang <mingfang@mac.com>
2026-05-20 21:40:44 +0000

    add barman

--- modules/cloudnative-pg-crd/main.tf
+++ modules/cloudnative-pg-crd/main.tf
@@ -1,6 +1,9 @@
 locals {
   # Read the embedded CNPG operator YAML manifest
   cnpg_manifest = file("${path.module}/cnpg-${var.operator_version}.yaml")
+
+  # Read the embedded Barman Cloud Plugin operator YAML manifest
+  barman_cloud_manifest = file("${path.module}/barman-cloud-${var.plugin_operator_version}.yaml")
 }
 
 resource "null_resource" "operator" {
@@ -24,5 +27,31 @@ resource "null_resource" "operator" {
   triggers = {
     operator_version = var.operator_version
     manifest_sha     = sha256(local.cnpg_manifest)
+  }
+}
+
+resource "null_resource" "barman_cloud_plugin" {
+  depends_on = [null_resource.operator]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      kubectl apply -f - <<'YAML'
+      ${local.barman_cloud_manifest}
+      YAML
+    EOT
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOT
+      kubectl delete -f - <<'YAML'
+      ${local.barman_cloud_manifest}
+      YAML
+    EOT
+  }
+
+  triggers = {
+    plugin_operator_version = var.plugin_operator_version
+    manifest_sha            = sha256(local.barman_cloud_manifest)
   }
 }
--- modules/cloudnative-pg-crd/variables.tf
+++ modules/cloudnative-pg-crd/variables.tf
@@ -7,3 +7,8 @@ variable "operator_version" {
   default     = "1.29.1"
   description = "CloudNativePG operator version to install. The corresponding YAML manifest must exist at cnpg-<version>.yaml in the module directory."
 }
+
+variable "plugin_operator_version" {
+  default     = "0.12.0"
+  description = "Barman Cloud Plugin operator version to install. The corresponding YAML manifest must exist at barman-cloud-<version>.yaml in the module directory."
+}
